4975d7cdf4cb0fd7554b29ca9cc5bd997271b18f
urielaero <aero.uriel@gmail.com>
2017-10-26 16:18:03 -0500

    fixed exports...

--- js/artool.js
+++ js/artool.js
@@ -168,5 +168,10 @@
     })
   }
 
-  window.ArTool = Ar;
+  var root = typeof self == 'object' && self.self === self && self ||
+            typeof global == 'object' && global.global === global && global ||
+            this ||
+            {};
+
+  root.ArTool = Ar;
 })(window);
